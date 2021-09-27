1d83bfd233b7ddaea8b5235f24678c490087fe94
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2021-09-27 20:41:41 +0545

    components_added

---
+++ resources/views/frontend/components/card.blade.php
@@ -0,0 +1,13 @@
+<div class="card bg-danger">
+    <div class="card-header">
+        @yield('head')
+    </div>
+    <div class="card-body">
+        <h5 class="card-title">
+            @yield('title')
+        </h5>
+        <p class="card-text">
+            @yield('text')
+        </p>
+    </div>
+</div>
--- resources/views/frontend/service.blade.php
+++ resources/views/frontend/service.blade.php
@@ -1,4 +1,25 @@
 @extends('frontend.template')
 @section('content')
- <h3>Service</h3>
+  <div class="row py-3 px-3">
+      <div class="col-md-4">
+       
+        @section('head')
+           <h3>Hello</h3>
+         @endsection
+         @include('frontend.components.card')
+      </div>
+      <div class="col-md-4">
+        @section('head')
+         <h3>Hello 2</h3>
+        @endsection
+        @include('frontend.components.card')
+      </div>
+     <div class="col-md-4">
+        @section('head')
+        <h3>Hello 3</h3>
+      @endsection
+        @include('frontend.components.card')
+     </div>
+  </div>
+ 
 @endsection
